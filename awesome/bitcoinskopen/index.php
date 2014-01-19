ff794076bf4820338f617b6ab8bf6b10dd25291e
WWWillems <lander.willems@gmail.com>
2014-01-19 22:54:59 +0100

    removed submodule

--- awesome/bitcoinskopen/index.php
+++ awesome/bitcoinskopen/index.php
@@ -1,3 +1,11 @@
+<?php session_start();
+    // store session data
+    $_SESSION['views']+=1;
+
+
+ ?>
+
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -16,20 +24,27 @@
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 
-        <?php
-            Print "Hello, World!";
-         ?>
+
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <form action="welcome.php" method="post">
+            Aantal: <input type="text" name="txtAmount"><br>
+            Adres: <input type="text" name="txtAddress"><br>
+            <input type="submit">
+        </form>
 
+        <?php
+            print("This page has been viewed " . getPageViews() . " times");
 
-
+            function getPageViews(){
+                return $_SESSION['views'];
+            }
+        ?>
+     
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
---
+++ awesome/bitcoinskopen/welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome! <br />>
+You are looking for <?php echo $_POST["txtAmount"]; ?> BTC <br />
+We'll send them to : <?php echo $_POST["txtAddress"]; ?>
+
+</body>
+</html>
